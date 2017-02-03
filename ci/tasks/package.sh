eedb8a6325d4e4c6539bad789360b525f7f31cce
System Administrator <root@Kshitizs-MacBook-Pro.local>
2017-02-03 14:35:40 -0700

    fix

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -7,4 +7,4 @@ cd movie-fun-cnc-source
 pwd
 ls
 ls ../
-mv build/get/applications/movie-fun-app/build/libs/movie-fun-app-1.1.0-SNAPSHOT.war ../package-output/movie-fun.war
+mv applications/movie-fun-app/build/libs/movie-fun-app-1.1.0-SNAPSHOT.war ../package-output/movie-fun.war
