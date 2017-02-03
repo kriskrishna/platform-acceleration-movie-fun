001e76a17b60b78a6c03aed2fb5b2a8abadbf6a7
System Administrator <root@Kshitizs-MacBook-Pro.local>
2017-02-03 16:41:47 -0700

    -latest-fix

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -3,11 +3,12 @@
 set -e +x
 
 mkdir -p package-output/applications/movie-fun-app/build/libs/
+ls package-output
 
 cd movie-fun-cnc-source
 ./gradlew clean build -xtest > /dev/null
 
 cp manifest.yml ../package-output/
-mv applications/movie-fun-app/build/libs/movie-fun-app-1.1.0-SNAPSHOT.war ../package-output/applications/movie-fun-app/build/libs/movie-fun.war
-mv applications/album-service/build/libs/album-service-1.1.0-SNAPSHOT.jar ../package-output/applications/album-service/build/libs/album-service.jar
-mv applications/movie-service/build/libs/movie-service-1.1.0-SNAPSHOT.jar ../package-output/applications/album-service/build/libs/movie-service.jar
+mv applications/movie-fun-app/build/libs/movie-fun-app-1.1.0-SNAPSHOT.war ../package-output/applications/movie-fun-app/build/libs/
+mv applications/album-service/build/libs/album-service-1.1.0-SNAPSHOT.jar ../package-output/applications/album-service/build/libs/
+mv applications/movie-service/build/libs/movie-service-1.1.0-SNAPSHOT.jar ../package-output/applications/album-service/build/libs/
