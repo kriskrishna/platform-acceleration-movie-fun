a1903eb6ae4f622e1950bce31d4c621705fce1a3
System Administrator <root@Kshitizs-MacBook-Pro.local>
2017-02-03 14:08:36 -0700

    fixed

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -3,4 +3,5 @@
 set -e +x
 
 cd movie-fun-cnc-source
-mv target/movie-fun-0.0.1-SNAPSHOT.jar ../package-output/movie-fun.jar
+./gradlew clean build > /dev/null
+mv applications/movie-fun-app/build/libs/movie-fun-app-1.1.0-SNAPSHOT.war ../package-output/movie-fun.war
