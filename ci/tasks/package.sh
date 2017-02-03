4222b17480e679f06660b3dada077ea6a4e01b1a
System Administrator <root@Kshitizs-MacBook-Pro.local>
2017-02-03 16:30:10 -0700

    fix

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,11 +2,11 @@
 
 set -e +x
 
+
 cd movie-fun-cnc-source
 ./gradlew clean build -xtest > /dev/null
-pwd
-ls applications/movie-fun-app/build/libs
-ls ../
-mv applications/movie-fun-app/build/libs/movie-fun-app-1.1.0-SNAPSHOT.war ../package-output/movie-fun.war
-mv applications/album-service/build/libs/album-service-1.1.0-SNAPSHOT.jar ../package-output/album-service.jar
-mv applications/movie-service/build/libs/movie-service-1.1.0-SNAPSHOT.jar ../package-output/movie-service.jar
+
+cp manifest.yml ../package-output/
+mv applications/movie-fun-app/build/libs/movie-fun-app-1.1.0-SNAPSHOT.war ../package-output/applications/movie-fun-app/build/libs/movie-fun.war
+mv applications/album-service/build/libs/album-service-1.1.0-SNAPSHOT.jar ../package-output/applications/album-service/build/libs/album-service.jar
+mv applications/movie-service/build/libs/movie-service-1.1.0-SNAPSHOT.jar ../package-output/applications/album-service/build/libs/movie-service.jar
