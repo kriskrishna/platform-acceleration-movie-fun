966850742382ee3038900881786510cb68dc1ec8
System Administrator <root@Kshitizs-MacBook-Pro.local>
2017-02-03 14:40:10 -0700

    fix

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -5,6 +5,6 @@ set -e +x
 cd movie-fun-cnc-source
 ./gradlew clean build -xtest > /dev/null
 pwd
-ls
+ls applications/movie-fun-app/build/libs
 ls ../
 mv applications/movie-fun-app/build/libs/movie-fun-app-1.1.0-SNAPSHOT.war ../package-output/movie-fun.war
